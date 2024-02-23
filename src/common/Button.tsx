3df7e9cbf33a4ff5d18a5a4df6974fcb3ac4f16c
Andrii <andrejveremcuk712@gmail.com>
2024-02-23 21:43:41 +0200

    feat: add disbled field to button props

--- src/common/Button.tsx
+++ src/common/Button.tsx
@@ -4,12 +4,26 @@ import {TouchableOpacity} from 'react-native';
 interface ButtonProps {
   onPress: () => void;
   className: string;
+  disabled?: boolean;
   children: ReactNode;
 }
 
-export const Button: FC<ButtonProps> = ({children, onPress, className}) => {
+export const Button: FC<ButtonProps> = ({
+  children,
+  onPress,
+  className,
+  disabled,
+}) => {
+  const handleButtonTouch = () => {
+    if (!!disabled) return;
+
+    onPress();
+  };
+
   return (
-    <TouchableOpacity onPress={onPress} className={className}>
+    <TouchableOpacity
+      onPress={handleButtonTouch}
+      className={`${className} ${disabled && 'bg-neutral-600 text-white'}`}>
       {children}
     </TouchableOpacity>
   );
