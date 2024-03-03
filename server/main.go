784f8a7fdcc2d9e3401cd2a2d9c7da7c5a046208
Andrii <andrejveremcuk712@gmail.com>
2024-03-03 13:13:06 +0200

    feat: add .env file

--- server/main.go
+++ server/main.go
@@ -1,1 +1,22 @@
- 
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+)
+
+
+func main() {
+
+	godotenv.Load(".env");
+
+	portString := os.Getenv("PORT");
+
+	if portString == "" {
+		log.Fatal("PORT is not found in the enviroment");
+	}
+
+	fmt.Println("PORT:", portString);
+}
